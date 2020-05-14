8b4e01659e05880357cced04553104dd79f35098
Luis miguel meza mendoza <zorkzone@gmail.com>
2020-05-13 20:08:07 -0500

    initialized

---
+++ app/src/main/java/com/lui2mi/validtest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lui2mi.validtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
